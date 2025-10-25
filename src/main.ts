325852e6deaa3607bf8b807e90b8ffcd6d4f9b30
Tomg <TheAutomaTom@gmail.com>
2025-10-25 16:42:52 -0600

    Fiddle with main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
 import "./styles/index.css";
 
 import App from "./App.vue";
-import router from "./router";
-
+import { createApp } from "vue";
 const app = createApp(App);
 
+import { createPinia } from "pinia";
 app.use(createPinia());
+
+import router from "./router";
 app.use(router);
 
 app.mount("#app");
